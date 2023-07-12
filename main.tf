ee07cf5c67f5440a7ce27a8eabd1650efaed7549
yunjae228 <yvkqzhs7@gmail.com>
2023-07-12 21:40:39 +0900

    terraform resource ?

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instace" "example" {
+  ami = "i~~~"
+
+  network_interface {
+    # ...
+  }
+
+}
+
+resource "aws_acm_certificate" "name" {
+
+
+}
+
+resource "aws_alb" "name" {
+
+}
